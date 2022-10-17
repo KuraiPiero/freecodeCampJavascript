570a78f2c1a7a83517f9ae39ba70c4575e95e547
KuraiPiero <54311200+KuraiPiero@users.noreply.github.com>
2022-10-17 11:52:29 -0500

    almos functional, failing with long words

--- testZone/script.js
+++ testZone/script.js
@@ -15,6 +15,25 @@
   };
 })();
 let show = console.log;
-// Setup
 
-// Setup
+function palindrome(str) {
+  let rigthSide = "";
+  let leftSide = "";
+
+  for (let i = 0; i < str.length + 1; i++) {
+    let rightIndex = parseInt(i);
+    if (RegExp(/^\p{L}/, "u").test(str[i])) {
+      if (str[rightIndex] != undefined) {
+        rigthSide += str[rightIndex];
+      }
+    }
+  }
+  for (let j = 1; j < str.length + 1; j++) {
+    if (RegExp(/^\p{L}/, "u").test(str[str.length - j])) {
+      leftSide += str[str.length - j];
+    }
+  }
+  show(rigthSide == leftSide);
+}
+
+palindrome("A man, a plan, a canal. Panama");
