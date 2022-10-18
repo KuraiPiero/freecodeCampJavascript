62059719f95ad51b935357070c85bd1fd4521d77
KuraiPiero <54311200+KuraiPiero@users.noreply.github.com>
2022-10-18 18:13:23 -0500

    new module for show funtion

--- testZone/script.js
+++ testZone/script.js
@@ -1,31 +1,16 @@
-(function () {
-  var old = console.log;
-  var logger = document.getElementById("newConsole");
-  console.log = function () {
-    for (var i = 0; i < arguments.length; i++) {
-      if (typeof arguments[i] == "object") {
-        logger.innerHTML +=
-          (JSON && JSON.stringify
-            ? JSON.stringify(arguments[i], undefined, 2)
-            : arguments[i]) + "<br />";
-      } else {
-        logger.innerHTML += arguments[i] + "<br />";
-      }
-    }
-  };
-})();
-let show = console.log;
-
+import show from "./console.js";
 function convertToRoman(num) {
-  let romanNumbers = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
-  let temp = num;
+  let romanNumbers = [
+    ["M", 1000],
+    ["D", 500],
+    ["C", 100],
+    ["L", 50],
+    ["X", 10],
+    ["V", 5],
+    ["I", 1],
+  ];
   let arr = [];
-  for (let nums in romanNumbers) {
-    if (temp > romanNumbers[nums]) {
-      arr.push(romanNumbers[nums]);
-      show((temp -= arr[0]));
-    }
-  }
+  let nump = 0;
+  let temp = num;
+  for (let i = 0; i < romanNumbers.length; i++) {}
 }
-
-convertToRoman(36);
