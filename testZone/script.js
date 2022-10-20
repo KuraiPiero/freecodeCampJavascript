27dc2b9f5d64ac57bdcbbd1f9b5716b3d4b9102a
KuraiPiero <54311200+KuraiPiero@users.noreply.github.com>
2022-10-20 09:03:22 -0500

    cleared workspace

--- testZone/script.js
+++ testZone/script.js
@@ -1,18 +1,1 @@
 import show from "./console.js";
-function convertToRoman(num) {
-  let romanNumbers = [
-    ["M", 1000],
-    ["D", 500],
-    ["C", 100],
-    ["L", 50],
-    ["X", 10],
-    ["V", 5],
-    ["I", 1],
-  ];
-  let arr = [];
-  let nump = 0;
-  let temp = num;
-  for (let i = 0; i < romanNumbers.length; i++) {}
-}
-
-show(45);
