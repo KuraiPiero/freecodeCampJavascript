42482ccc5b4987499c6615bb9dd37cc52b4bb2f0
KuraiPiero <54311200+KuraiPiero@users.noreply.github.com>
2022-10-28 23:31:56 -0500

    create a copy of the archives in order to edit the injection mode to old way

---
+++ objectPractice/oldFashionWay/console.js
@@ -0,0 +1,19 @@
+(function () {
+  var old = console.log;
+  var logger = document.getElementById("newConsole");
+  console.log = function () {
+    for (var i = 0; i < arguments.length; i++) {
+      if (typeof arguments[i] == "object") {
+        logger.innerHTML +=
+          (JSON && JSON.stringify
+            ? JSON.stringify(arguments[i], undefined, 2)
+            : arguments[i]) + "<br />";
+      } else {
+        logger.innerHTML += arguments[i] + "<br />";
+      }
+    }
+  };
+})();
+let show = console.log;
+
+export default show;
