1c1531abb646303852f98c6f7c4eecae52f24e49
KuraiPiero <54311200+KuraiPiero@users.noreply.github.com>
2022-11-07 19:43:57 -0500

    workspace cleaned

--- testZone/script.js
+++ testZone/script.js
@@ -1,20 +1,2 @@
 import show from "./console.js";
 document.querySelector("title").innerHTML = "Practica - funciones";
-
-const modifier = (text) => {
-  let paragraph = document.createElement("p");
-  paragraph.setAttribute("class", "newItem");
-  paragraph.innerHTML = text;
-  document.body.appendChild(paragraph);
-};
-
-function anotherModifier(color, weight, size, text) {
-  let elem = document.querySelector(".newItem").style;
-  elem.color = color;
-  elem.fontWeight = weight;
-  elem.fontSize = size;
-  document.querySelector(".newItem").innerHTML += text;
-}
-
-modifier("its created");
-anotherModifier("purple", 900, "3em", " </br> This is a new line");
